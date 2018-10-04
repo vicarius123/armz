e57d38635b52bf4456c55d51c71167f70ec30c0c
vicariusxz <cris_chong2@hotmail.com>
2018-10-04 14:15:17 +0300

    1

--- templates/armz/html/com_content/article/news.php
+++ templates/armz/html/com_content/article/news.php
@@ -10,8 +10,8 @@
 defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
+require_once dirname(__FILE__) . '/helper.php';
 
-// Create shortcuts to some parameters.
 $params  = $this->item->params;
 $images  = json_decode($this->item->images);
 $urls    = json_decode($this->item->urls);
@@ -19,17 +19,23 @@ $canEdit = $params->get('access-edit');
 $user    = JFactory::getUser();
 $info    = $params->get('info_block_position', 0);
 
+$lang = JFactory::getLanguage();
+$tag = $lang->getTag();
+
+
 // Check if associations are implemented. If they are, define the parameter.
 $assocParam = (JLanguageAssociations::isEnabled() && $params->get('show_associations'));
 JHtml::_('behavior.caption');
 $item = $this->item;
-$date = date('d.m.Y', strtotime($item->created));
+$date = $date = date('d', strtotime($item->created)).' '.JText::_(date('F', strtotime($item->created))).' '.date('Y', strtotime($item->created));
 ?>
-<div class="item-page-text <?=($this->pageclass_sfx !='reserve')? 'header-separation':'reserve-separation';?> <?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Article">
+<div class="item-page-text news-inner-page <?=($this->pageclass_sfx !='reserve')? 'header-separation':'reserve-separation';?> <?php echo $this->pageclass_sfx; ?>" itemscope itemtype="https://schema.org/Article">
 	<meta itemprop="inLanguage" content="<?php echo ($this->item->language === '*') ? JFactory::getConfig()->get('language') : $this->item->language; ?>" />
+	<div class="news-date"><span><?=$date;?></span></div>
 	<h1><?=$item->title;?></h1>
   <br>
-  <p class="news-date"><?=$date;?></p>
-	
-  <?=$item->fulltext;?>
+  <?=$item->text;?>
 </div>
+<pre>
+<? getMore($tag, $item->id);?>	
+</pre>
